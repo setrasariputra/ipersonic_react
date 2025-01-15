e1b0235ac7ecb8c09984d25ac8ff121eec7f3316
Wisnu K. Ardianto <setrasariputra@gmail.com>
2025-01-15 21:14:52 +0700

    Create page components

---
+++ src/Pages/Index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Index = () => {
+    return (
+        <>
+            <div>
+                <h1>Halaman Index</h1>
+            </div>
+        </>
+    )
+}
+
+export default Index;
---
+++ src/Pages/Result.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Result = () => {
+    return (
+        <>
+            <div>
+                <h1>Halaman Result</h1>
+            </div>
+        </>
+    )
+}
+
+export default Result;
